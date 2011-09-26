f9089f7e020eb8ef80f78d182ad790ef54f9d6d2
Matt Carroll <mjc1283@gmail.com>
2011-09-26 09:45:02 -0400

    plt login test

--- tests/belay-test.py
+++ tests/belay-test.py
@@ -17,6 +17,10 @@ CREATE_PLT_PW1FIELDID = "password1"
 CREATE_PLT_PW2FIELDID = "password2"
 CREATE_PLT_BUTTONID = "submit"
 
+LOGIN_PLT_UFIELD = "username"
+LOGIN_PLT_PFIELD = "password"
+LOGIN_PLT_SUBMIT = "submit"
+
 
 def error(driver, msg):
   print msg
@@ -75,6 +79,11 @@ def google_login_test(driver):
 
 
 def create_pltaccount_test(driver):
+  def clearall():
+    username_field.clear()
+    pw1_field.clear()
+    pw2_field.clear()
+
   def create_account(username, pw1, pw2):
     username_field.send_keys(username)
     pw1_field.send_keys(pw1)
@@ -92,8 +101,7 @@ def create_pltaccount_test(driver):
 
     if session_cookie_exists(driver):
       error(driver, "create_plt_test fail: mismatched pws")
-
-    clearall([username_field, pw1_field, pw2_field])
+    clearall()
 
   def shortpw_test():
     username_field.send_keys("joe")
@@ -106,7 +114,7 @@ def create_pltaccount_test(driver):
 
     if session_cookie_exists(driver):
       error(driver, "create_plt_test fail: short pw")
-    clearall([username_field, pw1_field, pw2_field])
+    clearall()
 
   def goodpw_test():
     username_field.send_keys("joe")
@@ -116,7 +124,6 @@ def create_pltaccount_test(driver):
 
     if not account_exists(driver, "joe"):
       error(driver, "create_plt_test fail: good pw")
-    clearall([username_field, pw1_field, pw2_field])
 
   driver.get(BELAY_URL)
 
@@ -136,10 +143,48 @@ def create_pltaccount_test(driver):
   print "create_pltaccount_test passed"
 
 
+def login_pltaccount_test(driver):
+  driver.delete_all_cookies()
+  driver.get(BELAY_URL)
+
+  link = get_elt(driver, "loginplt")
+  link.click()
+
+  ufield = get_elt(driver, LOGIN_PLT_UFIELD)
+  pfield = get_elt(driver, LOGIN_PLT_PFIELD)
+  submit = get_elt(driver, LOGIN_PLT_SUBMIT)
+
+  ufield.send_keys("joee")
+  pfield.send_keys("password123");
+  submit.click()
+  if session_cookie_exists(driver):
+    error(driver, "login_pltaccount_test fail: logged in with bad uname")
+  ufield.clear()
+  pfield.clear()
+
+  ufield.send_keys("joe")
+  pfield.send_keys("password1233")
+  submit.click()
+  if session_cookie_exists(driver):
+    error(driver, "login_pltaccount_test fail: logged in with bad pw")
+  ufield.clear()
+  pfield.clear()
+
+  ufield.send_keys("joe")
+  pfield.send_keys("password123")
+  submit.click()
+  if not session_cookie_exists(driver):
+    error(driver, "login_pltaccount_test fail: couldn't log in")
+
+  print "login_pltaccount_test passed"
+
+
 def main():
   driver = webdriver.Firefox()
+  driver.delete_all_cookies()
   google_login_test(driver)
   create_pltaccount_test(driver)
+  login_pltaccount_test(driver)
   driver.quit()
   
 
