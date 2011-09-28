8f23e639efec7948f38f58615440655c19e5713b
joseph <joe@cs.brown.edu>
2011-09-28 13:11:00 -0400

    clean some things up

--- dj-plt-belay/pltbelay/models.py
+++ dj-plt-belay/pltbelay/models.py
@@ -1,9 +1,11 @@
+import belaylibs.models as bcap
+
 from django.db import models
 
-class BelayAccount(models.Model):
+class BelayAccount(bcap.Grantable):
   station_url = models.CharField(max_length=200)
 
-class PltCredentials(models.Model):
+class PltCredentials(bcap.Grantable):
   username = models.CharField(max_length=200)
   salt = models.CharField(max_length=200)
   hashed_password = models.CharField(max_length=200)
@@ -16,7 +18,7 @@ class GoogleCredentials(models.Model):
   account = db.ReferenceProperty(BelayAccount, required=True)
 """
 
-class BelaySession(models.Model):
+class BelaySession(bcap.Grantable):
   session_id = models.CharField(max_length=200)
   account = models.ForeignKey(BelayAccount)
 
