a72c66a43baf7750c9a2664c7751384624d88d34
Matt Carroll <mjc1283@gmail.com>
2011-10-04 17:25:28 -0400

    wish re: granting to (handler, None), granted

--- dj-plt-belay/belaylibs/models.py
+++ dj-plt-belay/belaylibs/models.py
@@ -9,4 +9,4 @@ class Grant(models.Model):
   internal_path = models.CharField(max_length=200)
   # reference to DB item passed to cap handler
   #db_entity = db.ReferenceProperty(required=True)
-  db_entity = models.ForeignKey(Grantable, related_name="granted_entity")
+  db_entity = models.ForeignKey(Grantable, related_name="granted_entity", null=True)
