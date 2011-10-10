210ae78fa00096d19b84b0bb8018142221b553d5
joseph <joe@cs.brown.edu>
2011-10-10 19:38:24 -0400

    some cleanup to get ready for becomeInstance

--- dj-plt-belay/pltbelay/static/belay-frame.js
+++ dj-plt-belay/pltbelay/static/belay-frame.js
@@ -19,7 +19,12 @@ $(function() {
         $(document.body).append($("<div>Logged in</div>"));
         get_station(function(station) {
           console.log('Logged in: ', station);
-          go(station);
+          capServer.restore(station).get(function(station_info) {
+            go(station_info);
+          },
+          function(err) {
+            console.log('Couldn\'t get station info: ', err);
+          });
         });
       } else {
       }
@@ -29,7 +34,8 @@ $(function() {
     }
   });
 
-  function go(station_url) {
+  function go(station_info) {
+    console.log('station_info: ', station_info);
     var port = makePostMessagePort(window.parent, "belay");
     var tunnel = new CapTunnel(port);
     capServer.setResolver(function(instanceID) {
@@ -39,10 +45,13 @@ $(function() {
     });
     tunnel.setLocalResolver(function() { return capServer.publicInterface; });
     tunnel.sendOutpost(capServer.dataPreProcess({
-      becomeInstance: capServer.grant(function(launchInfo) {
+      becomeNewInstance: capServer.grant(function(launchInfo) {
         console.log('launchInfo: ', launchInfo);
         return 'why hello to you to!';
         // TODO: becomeAnInstance
+      }),
+      notifyLocation: capServer.grant(function(url) {
+        // TODO: get fragment, load private_data
       })
     }));
   }
