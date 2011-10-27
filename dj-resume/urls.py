8cf1d46812ddee7948ff2964675c854ac2854876
joseph <joe@cs.brown.edu>
2011-10-27 13:52:32 -0400

    un-apply

--- dj-resume/urls.py
+++ dj-resume/urls.py
@@ -5,13 +5,13 @@ from django.conf.urls.defaults import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-  (r'^$', 'apply.views.index_handler'),
+  (r'^$', 'resume.views.index_handler'),
   (r'cap/.*', 'belaylibs.dj_belay.proxyHandler'),
-  (r'applicant/', 'apply.views.applicant_handler'),
-  (r'new-account/', 'apply.views.new_account_handler'),
-  (r'admin/', 'apply.views.admin_handler'),
-  (r'review/', 'apply.views.review_handler'),
-  (r'appreview/', 'apply.view.appreview_handler'),
+  (r'applicant/', 'resume.views.applicant_handler'),
+  (r'new-account/', 'resume.views.new_account_handler'),
+  (r'admin/', 'resume.views.admin_handler'),
+  (r'review/', 'resume.views.review_handler'),
+  (r'appreview/', 'resume.view.appreview_handler'),
   (r'^common.js$', 'lib.py.common_js.common_js_handler')
 )
 
