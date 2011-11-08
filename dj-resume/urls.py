778cef51fb3f737db1738abb70e55561899c6984
Matt Carroll <mjc1283@gmail.com>
2011-11-08 09:21:49 -0500

    urls fix...
    They are regexps, so we have to be careful.
    Also, removed 'download CSV' option from resume.

--- dj-resume/urls.py
+++ dj-resume/urls.py
@@ -7,13 +7,13 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
   (r'cs/', 'resume.views.cs_index_handler'),
   (r'cap/.*', 'belaylibs.dj_belay.proxyHandler'),
-  (r'applicant/', 'resume.views.applicant_handler'),
-  (r'new-account/', 'resume.views.new_account_handler'),
-  (r'admin/', 'resume.views.admin_handler'),
-  (r'review/', 'resume.views.review_handler'),
-  (r'appreview/', 'resume.views.appreview_handler'),
-  (r'submit-reference/', 'resume.views.reference_handler'),
+  (r'^applicant/$', 'resume.views.applicant_handler'),
+  (r'^new-account/$', 'resume.views.new_account_handler'),
+  (r'^admin/$', 'resume.views.admin_handler'),
+  (r'^review/$', 'resume.views.review_handler'),
+  (r'^appreview/$', 'resume.views.appreview_handler'),
+  (r'^submit-reference/$', 'resume.views.reference_handler'),
   (r'^common.js$', 'lib.py.common_js.common_js_handler'),
-  (r'generate/', 'resume.generate.generate')
+  (r'^generate/$', 'resume.generate.generate')
 )
 
