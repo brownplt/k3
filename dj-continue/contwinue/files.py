1fbc9f2edcf7c998c16ba02bbb2f8d7a0388181e
joseph <joe@cs.brown.edu>
2011-12-11 21:24:34 -0500

    forgot an include

---
+++ dj-continue/contwinue/files.py
@@ -0,0 +1,27 @@
+import os
+import settings
+import logging
+from lib.py.common import logWith404
+from django.http import HttpResponse
+
+logger = logging.getLogger('default')
+
+def file_response(filename, response_name):
+    full_path = os.path.join(settings.SAVEDFILES_DIR, filename)
+
+    try:
+      full_file = open(full_path, 'r')
+      file_data = full_file.read()
+    except Exception as e:
+      return logWith404(logger,\
+        'file_response: exception reading file: %s' % e,\
+        level='error')
+
+    if file_data[0:4] == '%PDF':
+      mimetype = 'application/pdf'
+    else:
+      mimetype = 'application/octet-stream'
+
+    response = HttpResponse(file_data, mimetype=mimetype)
+    response['Content-Disposition'] = 'attachment; filename=%s' % response_name
+    return response
